d21dfd50c5e49172d5f90530d77358ae381d52b9
GuillaumeLandry <landry.guillaume00@hotmail.com>
2020-09-10 15:45:00 -0400

    Commit add dataconf

---
+++ dataconf.php
@@ -0,0 +1,9 @@
+<?php
+	// Remplir et renommer dataconf.php
+	define("BASEURL", "http://localhost/vino_etu/");
+
+	define('HOST', 'localhost');
+	define('USER', 'root');
+	define('PASSWORD', '');
+	define('DATABASE', 'dbvino');
+?>
